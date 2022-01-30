f7c0ed300fff6750dfc798daa566adbc67d25f4c
Bruno Silva <brunocs86l@outlook.com>
2022-01-30 13:27:34 -0300

    bad commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/ui/Header";
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Bruno Silva</div>;
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios("https://www.breakingbadapi.com/api/characte");
+
+      console.log(result.data);
+    };
+  }, []);
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
-
