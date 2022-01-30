5a78d29448ff39f5dfdd27349d4e2f4262d1e40e
Bruno Silva <brunocs86l@outlook.com>
2022-01-30 13:36:41 -0300

    rebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import Header from "./components/ui/Header";
 import "./App.css";
 
 const App = () => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      const result = await axios("https://www.breakingbadapi.com/api/characte");
-
-      console.log(result.data);
-    };
-  }, []);
-
   return (
     <div className="container">
       <Header />
